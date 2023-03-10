e195fdff9c14db48d0ba0e4b9360296524854a62
pySoo <soopy368@gmail.com>
2023-03-11 01:49:06 +0900

    [level1] 다트게임

---
+++ 프로그래머스/2018 카카오 블라인드/Level1.다트게임/다트게임.py
@@ -0,0 +1,22 @@
+def solution(dartResult):
+    scores = []
+    square_dict = { 'S' : 1, 'D' : 2, 'T' : 3 }
+    option_dict = { '*' : 2, '#' : -1 }
+    
+    dartResult = dartResult.replace('10', 'K')
+    
+    for s in dartResult:
+        if s.isdigit():
+            scores.append(int(s))
+        else:
+            if s == 'K':
+                scores.append(10)
+            elif s in square_dict:
+                scores[-1] **= square_dict[s]
+            else:
+                if s == '*':
+                    scores[-2:] = [x*2 for x in scores[-2:]]
+                else:
+                    scores[-1] *= option_dict[s]
+                    
+    return sum(scores)
