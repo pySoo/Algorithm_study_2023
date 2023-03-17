1635e5cc15addb1c5b240b8b6a9686c1c2914269
pySoo <soopy368@gmail.com>
2023-03-17 23:03:56 +0900

    [level2] 문자열 압축

---
+++ 프로그래머스/2020 카카오 블라인드/Level2. 문자열 압축/문자열 압축.py
@@ -0,0 +1,26 @@
+def solution(s):
+    answer = []
+    
+    if len(s) == 1:
+        return 1
+    
+    for i in range(1, len(s)//2 + 1):
+        result = ''
+        tmp = s[:i]
+        cnt = 1
+        for j in range(i, len(s), i):
+            if tmp == s[j:j+i]:
+                cnt += 1
+            else:
+                if cnt > 1:
+                    result += str(cnt) + tmp
+                else:
+                    result += tmp
+                cnt = 1
+                tmp = s[j:j+i]
+        if cnt > 1:
+            result += str(cnt) + tmp
+        else:
+            result += tmp
+        answer.append(len(result))
+    return min(answer)
